00b7e7f827078e76c1bca46298bd0d155df85af6
Sally.N.Ngure <nguresallynjoki@gmail.com>
2025-06-23 22:31:24 +0300

    my commit before switching branches

--- _config.js
+++ _config.js
@@ -2,8 +2,8 @@ var config = {}
 
 // Update to have your correct username and password
 config.mongoURI = {
-    production: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom?retryWrites=true&w=majority',
-    development: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom-dev?retryWrites=true&w=majority',
-    test: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom-test?retryWrites=true&w=majority',
+    production: 'mongodb+srv://salita:Jabulani@0793835080@devops10.e6q9xjo.mongodb.net/?retryWrites=true&w=majority&appName=DevOps10',
+    development: 'mmongodb+srv://salita:Jabulani@0793835080@devops10.e6q9xjo.mongodb.net/?retryWrites=true&w=majority&appName=DevOps10',
+    test: 'mongodb+srv://salita:Jabulani@0793835080@devops10.e6q9xjo.mongodb.net/?retryWrites=true&w=majority&appName=DevOps10',
 }
 module.exports = config;
